4eb78810e664d54daf6d902f4e8e94b5c200480e
miny0512 <nameless1029@naver.com>
2023-11-01 18:15:59 +0900

    [Feat] Json 암호화, 복호화 추가

--- HoneyDragonProject/Assets/00_Scripts/Runtime/Util/Utility.cs
+++ HoneyDragonProject/Assets/00_Scripts/Runtime/Util/Utility.cs
@@ -1,6 +1,8 @@
 ﻿using RPG.Core;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 using UnityEngine;
 
 namespace RPG.Util
@@ -54,6 +56,60 @@ namespace RPG.Util
             Vector2 sprial = GetSpiralPosition(t, speed, factor);
             Vector3 result = new Vector3(position.x + sprial.x, position.y, position.z + sprial.y);
             return result;
+        }
+    }
+
+    public static class JsonUtil
+    {
+        public static string Decrypt(string textToDecrypt, string key)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+
+            rijndaelCipher.KeySize = 128;
+            rijndaelCipher.BlockSize = 128;
+
+            byte[] encryptedData = Convert.FromBase64String(textToDecrypt);
+            byte[] pwdBytes = Encoding.UTF8.GetBytes(key);
+            byte[] keyBytes = new byte[16];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length)
+            {
+                len = keyBytes.Length;
+            }
+
+            Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            rijndaelCipher.IV = keyBytes;
+
+            byte[] plainText = rijndaelCipher.CreateDecryptor().TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+            return Encoding.UTF8.GetString(plainText);
+        }
+
+        public static string Encrypt(string textToEncrypt, string key)
+        {
+            RijndaelManaged rijndaelCipher = new RijndaelManaged();
+            rijndaelCipher.Mode = CipherMode.CBC;
+            rijndaelCipher.Padding = PaddingMode.PKCS7;
+
+            rijndaelCipher.KeySize = 128;
+            rijndaelCipher.BlockSize = 128;
+            byte[] pwdBytes = Encoding.UTF8.GetBytes(key);
+            byte[] keyBytes = new byte[16];
+            int len = pwdBytes.Length;
+            if (len > keyBytes.Length)
+            {
+                len = keyBytes.Length;
+            }
+
+            Array.Copy(pwdBytes, keyBytes, len);
+            rijndaelCipher.Key = keyBytes;
+            rijndaelCipher.IV = keyBytes;
+            ICryptoTransform transform = rijndaelCipher.CreateEncryptor();
+            byte[] plainText = Encoding.UTF8.GetBytes(textToEncrypt);
+            return Convert.ToBase64String(transform.TransformFinalBlock(plainText, 0, plainText.Length));
         }
     }
 
